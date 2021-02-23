e34ca5bf4cf68b3174ea7e2637fc414bf768dba5
Zina Mohamed <zina.a.mohamed@gmail.com>
2021-02-22 17:06:37 -0800

    TDD post

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -91,5 +91,49 @@ describe('app routes', () => {
 
       expect(data.body).toEqual(expectation);
     });
+    
+    test('creates a new product and that new product is in our product list', async() => {
+      // define the new product we want create
+      const newProduct = { 
+        'id':4,
+        'image': 'https://media2.giphy.com/media/aMO3eGc9frP9i24AqG/source.gif',
+        'name': 'Alpha Arbutin', 
+        'size': '12ml',
+        'price': '$12',
+        'type': 'Serum'
+        
+      };
+
+      // define what we expect that product to look like after SQL does its thing
+      const expectedProduct = {
+        ...newProduct,
+        'id': 4,
+        'owner_id': 1,
+      };
+
+      // use the post endpoint to create a product
+      const data = await fakeRequest(app)
+        .post('/products')
+        // pass in our new product as the req.body
+        .send(newProduct)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      // we expect the post endpoint to responds with our expected product
+      expect(data.body).toEqual(expectedProduct);
+
+      //  check that the new product is in DB
+      const allProducts = await fakeRequest(app)
+        // fetch all the products
+        .get('/products')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      // find Alpha Arbutin
+      const alphaArbutin = allProducts.body.find(product => product.name === 'Alpha Arbutin');
+
+      // check to see that Alpha Arbutin matches the one we expected
+      expect(alphaArbutin).toEqual(expectedProduct);
+    });
   });
 });
